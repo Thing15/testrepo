b1c091101e806f33c21f933248f9781199cee6e6
BT_1N3_05 <Boryslavw@nycstudents.net>
2025-01-10 11:03:39 -0500

    merge problem

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("our first shared project");
+        System.out.println("merge conflict");
         System.out.print("this is the other partner adding some code");
         System.out.println("what's up");
+        System.out.println("not much!");
     }
 }
