13302c693c3361a073e2e4002772baf99ba082f3
BT_1N3_05 <Boryslavw@nycstudents.net>
2025-01-10 10:43:57 -0500

    partner 2 adding code

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("our first shared project");
+        System.out.print("this is the other partner adding some code");
     }
 }
