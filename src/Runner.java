069de6f2b526760711342b4ec4c136cc4006ab9b
BT_1N3_06 <keviny119@nycstudents.net>
2025-01-10 11:02:36 -0500

    added print statement and a new method

--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,6 @@ public class Runner {
         System.out.println("our first shared project");
         System.out.print("this is the other partner adding some code");
         System.out.println("what's up");
+        System.out.println("not much!");
     }
 }
