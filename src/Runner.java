07b55bc73ae925c7ff51af90a6df7b42c34afd31
BT_1N3_05 <Boryslavw@nycstudents.net>
2025-01-13 10:00:37 -0500

    #71

--- src/Runner.java
+++ src/Runner.java
@@ -3,6 +3,6 @@ public class Runner {
         System.out.println("merge conflict");
         System.out.print("this is the other partner adding some code");
         System.out.println("what's up");
-        System.out.println("not much!");
+        System.out.println("Partner 2 is changing this line");
     }
 }
