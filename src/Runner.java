3f85415960e58c1973a70677851d6aebf26b425c
BT_1N3_05 <Boryslavw@nycstudents.net>
2025-01-13 10:02:16 -0500

    #73

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("merge conflict");
         System.out.print("partner 1 just changed this string");
-        System.out.println("what's up");
+        System.out.println("LOOK AT ME");
         System.out.println("Partner 2 is changing this line");
     }
 }
