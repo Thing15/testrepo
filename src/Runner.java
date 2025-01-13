30ba3b34cc7da999eb62c743ddc925d46b4364ab
BT_1N3_06 <keviny119@nycstudents.net>
2025-01-13 10:00:40 -0500

    change

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("merge conflict");
-        System.out.print("this is the other partner adding some code");
+        System.out.print("partner 1 just changed this string");
         System.out.println("what's up");
         System.out.println("not much!");
     }
